77d5657732d87c588c599f15aec281becf8ae461
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-09-03 22:33:32 +0300

    Create Pages And Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SingUp.jsx';  // Corrected spelling
+import Projects from './pages/Projects.jsx';
+import About from './pages/About.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1 className='bg-black'>Hello From My Blog Website Using Mern Stack and Tailwind Css</h1>
-      
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
