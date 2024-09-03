84bb4df64f200dc0707f34d9d3ebbe01de2152f2
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-09-03 21:46:39 +0300

    install important packages

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='bg-black'>Hello From My Blog Website Using Mern Stack and Tailwind Css</h1>
+      
+    </div>
+  )
+}
